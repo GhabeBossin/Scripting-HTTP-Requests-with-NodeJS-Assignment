a4ad4a9d09ddda40bc1d5ee54cbf745157bc682c
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-14 22:33:59 +0000

    added print_html.js finished step 2 printing buffered strings with += adding chunks of data to var body, logged on 'end' response function

---
+++ print_buffered_strings/print_html.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    var body = '';
+
+    response.on('data', function(chunk) {
+      body += chunk;
+      return;
+    });
+
+    response.on('end', function(){
+      console.log(body);
+    })
+  });
+}
+getAndPrintHTML();
