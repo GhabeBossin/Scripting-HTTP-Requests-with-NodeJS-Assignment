64f1c1abf8f096c46430ff0874181781908c2762
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-15 00:12:34 +0000

    Step 3 currently working as per instructions in compass, added options param to getAndPrintHTML func, referencing the requestOptions object values now stored outside the function. However, pulled down same HTML in the test as part two so I will confirm if this is correct before proceeding

--- print_parameters/print_param.js
+++ print_parameters/print_param.js
@@ -1,16 +1,40 @@
 var https = require('https');
-var myArgs = process.argv.slice(2);
-var repOwner = process.argv.slice[3];
-var repoName = process.argv.slice[4];
+// var myArgs = process.argv.slice(2);
+// var repoOwner = process.argv.slice[3];
+// var repoName = process.argv.slice[4];
 
-function getAndPrintHTML () {
+//FAIL -scope test
+// var options = {
+//   host: 'sytantris.github.io',
+//   path: '/http-examples/step2.html'
+// };
 
-  var requestOptions = {
-    host: '',
-    path: ''
+//FAIL -scope test 2
+// var options = {
+//   host: requestOptions[0],
+//   path: requestOptions
+// };
+
+//FAIL -argv test
+// var requestOptions = {
+//   host: repoOwner,
+//   path: repoName
+// };
+
+function getAndPrintHTML (options) {
+  //MOVED FROM
+  // var requestOptions = {
+  //   host: 'repoOwner',
+  //   path: 'repoName'
+  // };
+
+  //WORKING?? ===========^^^^ ADD [options] back in as a param above to work
+  options = {
+    host: requestOptions.host,
+    path: requestOptions.path
   };
-
-  https.get(requestOptions, function(response) {
+  // for WORKING change requestOptions back to options
+  https.get(options, function(response) {
     response.setEncoding('utf8');
     var body = '';
 
@@ -19,9 +43,16 @@ function getAndPrintHTML () {
       return;
     });
 
-    response.on('end', function(){
+    response.on('end', function() {
       console.log(body);
-    })
+    });
   });
 }
+
+//MOVED TO HERE FOR PART 3--- WORKING
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step2.html'
+};
+
 getAndPrintHTML();
