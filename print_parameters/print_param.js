22c6b0f3d9036caff62f77f6a42524f55a4bad12
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-15 00:24:53 +0000

    Step 3 confirmed with mentor, tidied up comments/tests

--- print_parameters/print_param.js
+++ print_parameters/print_param.js
@@ -1,39 +1,13 @@
 var https = require('https');
-// var myArgs = process.argv.slice(2);
-// var repoOwner = process.argv.slice[3];
-// var repoName = process.argv.slice[4];
-
-//FAIL -scope test
-// var options = {
-//   host: 'sytantris.github.io',
-//   path: '/http-examples/step2.html'
-// };
-
-//FAIL -scope test 2
-// var options = {
-//   host: requestOptions[0],
-//   path: requestOptions
-// };
-
-//FAIL -argv test
-// var requestOptions = {
-//   host: repoOwner,
-//   path: repoName
-// };
 
 function getAndPrintHTML (options) {
-  //MOVED FROM
-  // var requestOptions = {
-  //   host: 'repoOwner',
-  //   path: 'repoName'
-  // };
 
-  //WORKING?? ===========^^^^ ADD [options] back in as a param above to work
+  //WORKING
   options = {
     host: requestOptions.host,
     path: requestOptions.path
   };
-  // for WORKING change requestOptions back to options
+  //WORKING
   https.get(options, function(response) {
     response.setEncoding('utf8');
     var body = '';
@@ -49,7 +23,6 @@ function getAndPrintHTML (options) {
   });
 }
 
-//MOVED TO HERE FOR PART 3--- WORKING
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step2.html'
